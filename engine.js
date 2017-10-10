d2a11636b8550b849e744ec8fe03939b77bd88f3
jakub.ksiazek <jakub.ksiazek@mediaflex.pl>
2017-10-10 16:38:29 +0200

    make Graph object

--- engine.js
+++ engine.js
@@ -1,178 +1,141 @@
-// zasób potrzebny do tworzenia obiektów svg:
-var svg = "http://www.w3.org/2000/svg";
+var Graph = {
+    graph: document.getElementById("graph"),
+    svg: "http://www.w3.org/2000/svg",
+    counter: 0,
+    init: function () {
+        this.createCircle(180, 270, 90, ++this.counter);
+        this.createText(172, 310, this.counter);
+    },
+    createCircle: function (x, y, space, id) {
+        var circle = document.createElementNS(this.svg, "circle");
 
-// kontener dla obiektów svg:
-var graph = document.getElementById("graph");
+        circle.setAttributeNS("", "cx", x);
+        circle.setAttributeNS("", "cy", y);
+        circle.setAttributeNS("", "r", 20);
+        circle.setAttributeNS("", "stroke", "black");
+        circle.setAttributeNS("", "stroke-width", 3);
+        circle.setAttributeNS("", "fill", "gray");
+        circle.setAttributeNS("", "id", "z" + id);
+        circle.setAttributeNS("", "data-space", space);
+        circle.setAttributeNS("", "onclick", "Graph.createNode(evt, 'left', " + space + ")");
 
-// licznik dla węzłów antydrzewa:
-var counter = 0;
+        this.graph.appendChild(circle);
+    },
+    createLine: function (x1, y1, x2, y2) {
+        var line = document.createElementNS(this.svg, "line");
+        line.setAttributeNS("", "x1", x1);
+        line.setAttributeNS("", "y1", y1);
+        line.setAttributeNS("", "x2", x2);
+        line.setAttributeNS("", "y2", y2);
+        line.setAttributeNS("", "stroke", "black");
+        line.setAttributeNS("", "stroke-width", 3);
+        line.setAttributeNS("", "marker-end", "url(#arrow)");
 
-/*
- *	Funkcja rysująca koło.
- *
- *	@x, @y - współrzędne koła
- */
-function createCircle(x, y, space, id) {
-	var circle = document.createElementNS(svg, "circle");
+        this.graph.appendChild(line);
+    },
+    createText: function (x, y, content) {
+        var text = document.createElementNS(this.svg, "text");
+        text.setAttributeNS("", "x", x);
+        text.setAttributeNS("", "y", y);
+        text.setAttributeNS("", "fill", "white");
+        text.setAttributeNS("", "font-size", "16");
+        text.setAttributeNS("", "font-weight", "bold");
 
-	circle.setAttributeNS("","cx", x);
-	circle.setAttributeNS("","cy", y);
-	circle.setAttributeNS("","r", 20);
-	circle.setAttributeNS("","stroke", "black");
-	circle.setAttributeNS("","stroke-width", 3);
-	circle.setAttributeNS("","fill", "gray");
-	circle.setAttributeNS("","id", "z" + id);
-	circle.setAttributeNS("","data-space", space);
-	circle.setAttributeNS("","onclick", "createNode(evt, 'left', " + space + ")");
+        var textNode = document.createTextNode(content);
+        text.appendChild(textNode);
 
-	graph.appendChild(circle);
-}
+        this.graph.appendChild(text);
+    },
+    createNode: function (evt, direction, space) {
+        var target = evt.target;
+        var y = parseInt(target.getAttribute("cy"));
+        var x = parseInt(target.getAttribute("cx"));
 
-/*
- *	Funkcja rysująca odcinek.
- *
- *	@x1, y1 - współrzędne początku odcinka.
- *	@x2, y2 - współrzędne końca odcinka.
- */
-function createLine(x1, y1, x2, y2) {
-	var line = document.createElementNS(svg, "line");
-	line.setAttributeNS("","x1", x1);
-	line.setAttributeNS("","y1", y1);
-	line.setAttributeNS("","x2", x2);
-	line.setAttributeNS("","y2", y2);
-	line.setAttributeNS("","stroke", "black");
-	line.setAttributeNS("","stroke-width", 3);
-	line.setAttributeNS("","marker-end", "url(#arrow)");
+        if (direction == "left") {
+            target.setAttribute("onclick", "Graph.createNode(evt, 'right', " + space + ")");
 
-	graph.appendChild(line);
-}
+            var cx = x - space;
+            var cy = y - 80;
+            x -= 5;
 
-/*
- *	Funkcja wypisująca tekst
- *
- *	@x, y - współrzędne tekstu
- *	@content - tekst do wypisania
- */
-function createText(x, y, content) {
-	var text = document.createElementNS(svg, "text");
+        } else {
+            target.removeAttribute("onclick");
 
-	text.setAttributeNS("", "x", x);
-	text.setAttributeNS("", "y", y);
-	text.setAttributeNS("", "fill", "white");
-	text.setAttributeNS("", "font-size", "16");
-	text.setAttributeNS("", "font-weight", "bold");
+            var cx = x + space;
+            var cy = y - 80;
+            x += 5;
+        }
 
-	var textNode = document.createTextNode(content);
-	text.appendChild(textNode);
+        if (cy > 0) {
+            this.createLine(x, y - 20, cx, cy);
+            this.createCircle(cx, cy, space / 2, ++this.counter);
+            this.createText(cx - 8, cy + 40, this.counter);
+        } else {
+            target.removeAttribute("onclick");
+        }
+    },
+    run: function(tasks, p1, p2, parent) {
+        tasks.sort(function (a, b) {
+            if (a.getAttribute('data-space') < b.getAttribute('data-space')) {
+                return 1;
+            } else if (a == b) {
+                return 0;
+            } else {
+                return -1;
+            }
+        });
 
-	graph.appendChild(text);
-}
+        tasks = tasks.toArray();
+        var k1 = 0;
+        var k2 = 0;
 
-/*
- *	Funkcja rysująca węzeł.
- *
- *	@evt - zdarzenie kliku
- *	@direction - kierunek wygenerowania węzła
- */
-function createNode(evt, direction, space) {
-	var target = evt.target;
-	var y = parseInt(target.getAttribute("cy"));
-	var x = parseInt(target.getAttribute("cx"));
+        var baer = setInterval(function () {
+            var task = tasks.pop();
 
-	// sprawdza, z której strony powinien zostać narysowany nowy węzeł i wyznacza jego współrzędne
-	if(direction == "left") {
-		target.setAttribute("onclick", "createNode(evt, 'right', " + space + ")");
+            if (tasks.length == 0) {
+                var children = p1.children();
+                k1 += 1;
+            } else {
+                if (k1 - 1 == k2) {
+                    var children = p2.children();
+                    k2 += 2;
+                } else {
+                    var children = $("#p1").children();
+                    k1 += 1;
+                }
+            }
 
-		var cx = x - space;
-		var cy = y - 80;
-		x -= 5;
+            var node = document.getElementById(task.getAttribute('id'));
+            node.setAttribute('fill', 'red');
 
-	} else {
-		target.removeAttribute("onclick");
+            for (var i = 0; i < children.length; i++) {
+                if (!$(children[i]).html()) {
+                    $(children[i]).html('Z <sub>' + task.getAttribute('id').substr(1) + '</sub>');
+                    break;
+                }
+            }
 
-		var cx = x + space;
-		var cy = y - 80;
-		x += 5;
-	}
+            if (tasks.length == 0) {
+                parent.html(k1);
+                parent.parent().show();
+                clearInterval(baer);
+            }
+        }, 1200, tasks, k1, k2);
+    }
+};
 
-	// sprawdza czy węzeł nie wychodzi za obszar rysowania
-	if(cy > 0) {
-		createLine(x, y - 20, cx, cy);
-		createCircle(cx, cy, space/2, ++counter);
-		createText(cx - 8, cy + 40, counter);
-	} else {
-		target.removeAttribute("onclick");
-	}
-}
+$(document).ready(function () {
+    Graph.init();
 
-$(document).ready(function() {
-	// rysowanie pierwszego węzła
-	createCircle(180, 270, 90, ++counter);
-	createText(172, 310, counter);
+    $(".baer-button").click(function () {
+        var tasks = $("circle");
 
-	/*
-	 *	Wydarzenie uruchamiające działanie algorytmu Baera.
-	 */
-	$(".baer-button").click(function() {
-		// kontener z zadaniami
-		var tasks = $("circle");
+        tasks.each(function (index, element) {
+            $(element).removeAttr("onclick", "");
+        });
 
-		// blokowanie dalszej rozbudowy antydrzewa
-		tasks.each(function(index, element) {
-			$(element).removeAttr("onclick", "");
-		});
+        Graph.run(tasks, $("#p1"), $("#p2"), $("#c_max"));
 
-		// ukrycie przycisku
-		$(this).fadeOut();
-
-		// sortowanie kontenera z zadaniami
-		tasks.sort(function(a, b) {
-			if(a.getAttribute('data-space') < b.getAttribute('data-space')) {
-				return 1;
-			} else if(a == b) {
-				return 0;
-			} else {
-				return -1;
-			}
-		});
-
-		// przygotowanie zmiennych do algorytmu baera
-		tasks = tasks.toArray();
-		var k1 = 0;
-		var k2 = 0;
-
-		// właściwy kod algorytmu Baera
-		var baer = setInterval(function() {
-			var task = tasks.pop();
-
-			if(tasks.length == 0) {
-				var children = $("#p1").children();
-				k1 += 1;
-			} else {
-				if(k1 - 1 == k2) {
-					var children = $("#p2").children();
-					k2 += 2;
-				} else {
-					var children = $("#p1").children();
-					k1 += 1;
-				}
-			}
-
-			var node = document.getElementById(task.getAttribute('id'));
-			node.setAttribute('fill', 'red');
-
-			for(var i = 0; i < children.length; i++) {
-				if(!$(children[i]).html()) {
-					$(children[i]).html('Z <sub>' + task.getAttribute('id').substr(1) + '</sub>');
-					break;
-				}
-			}
-
-			if(tasks.length == 0) {
-				$("#c_max").html(k1);
-				$("#baer_equal").show();
-				clearInterval(baer);
-			}
-		}, 1500, tasks, k1, k2);
-	});
+        $(this).fadeOut();
+    });
 });
