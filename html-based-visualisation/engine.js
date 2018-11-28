63dced5860a3e07c3d16d724bdfa478b94b90257
Jakub <valvadis@o2.pl>
2018-11-28 20:33:01 +0100

    refactor engine

--- html-based-visualisation/engine.js
+++ html-based-visualisation/engine.js
@@ -1,141 +1,147 @@
-var Graph = {
-    graph: document.getElementById("graph"),
-    svg: "http://www.w3.org/2000/svg",
-    counter: 0,
-    init: function () {
-        this.createCircle(180, 270, 90, ++this.counter);
-        this.createText(172, 310, this.counter);
-    },
-    createCircle: function (x, y, space, id) {
-        var circle = document.createElementNS(this.svg, "circle");
+$(function() {
+    'use strict';
 
-        circle.setAttributeNS("", "cx", x);
-        circle.setAttributeNS("", "cy", y);
-        circle.setAttributeNS("", "r", 20);
-        circle.setAttributeNS("", "stroke", "black");
-        circle.setAttributeNS("", "stroke-width", 3);
-        circle.setAttributeNS("", "fill", "gray");
-        circle.setAttributeNS("", "id", "z" + id);
-        circle.setAttributeNS("", "data-space", space);
-        circle.setAttributeNS("", "onclick", "Graph.createNode(evt, 'left', " + space + ")");
+    window.Graph = {
+        graph: document.getElementById('graph'),
+        svg: 'http://www.w3.org/2000/svg',
+        counter: 0,
+        init: function () {
+            this.createCircle(180, 270, 90, ++this.counter);
+            this.createText(172, 310, this.counter);
+        },
+        createCircle: function (x, y, space, id) {
+            let circle = document.createElementNS(this.svg, 'circle');
 
-        this.graph.appendChild(circle);
-    },
-    createLine: function (x1, y1, x2, y2) {
-        var line = document.createElementNS(this.svg, "line");
-        line.setAttributeNS("", "x1", x1);
-        line.setAttributeNS("", "y1", y1);
-        line.setAttributeNS("", "x2", x2);
-        line.setAttributeNS("", "y2", y2);
-        line.setAttributeNS("", "stroke", "black");
-        line.setAttributeNS("", "stroke-width", 3);
-        line.setAttributeNS("", "marker-end", "url(#arrow)");
+            circle.setAttributeNS('', 'cx', x);
+            circle.setAttributeNS('', 'cy', y);
+            circle.setAttributeNS('', 'r', 20);
+            circle.setAttributeNS('', 'stroke', 'black');
+            circle.setAttributeNS('', 'stroke-width', 3);
+            circle.setAttributeNS('', 'fill', 'gray');
+            circle.setAttributeNS('', 'id', 'z' + id);
+            circle.setAttributeNS('', 'data-space', space);
+            circle.setAttributeNS('', 'onclick', 'Graph.createNode(event, \'left\', ' + space + ')');
 
-        this.graph.appendChild(line);
-    },
-    createText: function (x, y, content) {
-        var text = document.createElementNS(this.svg, "text");
-        text.setAttributeNS("", "x", x);
-        text.setAttributeNS("", "y", y);
-        text.setAttributeNS("", "fill", "white");
-        text.setAttributeNS("", "font-size", "16");
-        text.setAttributeNS("", "font-weight", "bold");
+            this.graph.appendChild(circle);
+        },
+        createLine: function (x1, y1, x2, y2) {
+            let line = document.createElementNS(this.svg, 'line');
 
-        var textNode = document.createTextNode(content);
-        text.appendChild(textNode);
+            line.setAttributeNS('', 'x1', x1);
+            line.setAttributeNS('', 'y1', y1);
+            line.setAttributeNS('', 'x2', x2);
+            line.setAttributeNS('', 'y2', y2);
+            line.setAttributeNS('', 'stroke', 'black');
+            line.setAttributeNS('', 'stroke-width', 3);
+            line.setAttributeNS('', 'marker-end', 'url(#arrow)');
 
-        this.graph.appendChild(text);
-    },
-    createNode: function (evt, direction, space) {
-        var target = evt.target;
-        var y = parseInt(target.getAttribute("cy"));
-        var x = parseInt(target.getAttribute("cx"));
+            this.graph.appendChild(line);
+        },
+        createText: function (x, y, content) {
+            let text = document.createElementNS(this.svg, 'text');
 
-        if (direction == "left") {
-            target.setAttribute("onclick", "Graph.createNode(evt, 'right', " + space + ")");
+            text.setAttributeNS('', 'x', x);
+            text.setAttributeNS('', 'y', y);
+            text.setAttributeNS('', 'fill', 'white');
+            text.setAttributeNS('', 'font-size', '16');
+            text.setAttributeNS('', 'font-weight', 'bold');
 
-            var cx = x - space;
-            var cy = y - 80;
-            x -= 5;
+            let textNode = document.createTextNode(content);
+            text.appendChild(textNode);
 
-        } else {
-            target.removeAttribute("onclick");
+            this.graph.appendChild(text);
+        },
+        createNode: function (event, direction, space) {
+            let target = event.target,
+                y = parseInt(target.getAttribute('cy')),
+                x = parseInt(target.getAttribute('cx')),
+                cx = null, cy = null;
 
-            var cx = x + space;
-            var cy = y - 80;
-            x += 5;
-        }
+            if (direction === 'left') {
+                target.setAttribute('onclick', 'Graph.createNode(event, \'right\', ' + space + ')');
 
-        if (cy > 0) {
-            this.createLine(x, y - 20, cx, cy);
-            this.createCircle(cx, cy, space / 2, ++this.counter);
-            this.createText(cx - 8, cy + 40, this.counter);
-        } else {
-            target.removeAttribute("onclick");
-        }
-    },
-    run: function(tasks, p1, p2, parent) {
-        tasks.sort(function (a, b) {
-            if (a.getAttribute('data-space') < b.getAttribute('data-space')) {
-                return 1;
-            } else if (a == b) {
-                return 0;
+                cx = x - space;
+                cy = y - 80;
+                x -= 5;
+
             } else {
-                return -1;
+                target.removeAttribute('onclick');
+
+                cx = x + space;
+                cy = y - 80;
+                x += 5;
             }
-        });
 
-        tasks = tasks.toArray();
-        var k1 = 0;
-        var k2 = 0;
-
-        var baer = setInterval(function () {
-            var task = tasks.pop();
-
-            if (tasks.length == 0) {
-                var children = p1.children();
-                k1 += 1;
+            if (cy > 0) {
+                this.createLine(x, y - 20, cx, cy);
+                this.createCircle(cx, cy, space / 2, ++this.counter);
+                this.createText(cx - 8, cy + 40, this.counter);
             } else {
-                if (k1 - 1 == k2) {
-                    var children = p2.children();
+                target.removeAttribute('onclick');
+            }
+        },
+        run: function(tasks, p1, p2, parent) {
+            let k1 = 0, k2 = 0, baer = null, children = null;
+
+            tasks.sort(function (a, b) {
+                if (a.getAttribute('data-space') < b.getAttribute('data-space')) {
+                    return 1;
+                } else if (a === b) {
+                    return 0;
+                } else {
+                    return -1;
+                }
+            });
+
+            tasks = tasks.toArray();
+
+            baer = setInterval(function () {
+                let task = tasks.pop(),
+                    node = null;
+
+                if (tasks.length === 0) {
+                    children = p1.children();
+                    k1 += 1;
+                } else if (k1 - 1 === k2) {
+                    children = p2.children();
                     k2 += 2;
                 } else {
-                    var children = $("#p1").children();
+                    children = $('#p1').children();
                     k1 += 1;
                 }
-            }
 
-            var node = document.getElementById(task.getAttribute('id'));
-            node.setAttribute('fill', 'red');
+                node = document.getElementById(task.getAttribute('id'));
+                node.setAttribute('fill', 'red');
 
-            for (var i = 0; i < children.length; i++) {
-                if (!$(children[i]).html()) {
-                    $(children[i]).html('Z <sub>' + task.getAttribute('id').substr(1) + '</sub>');
-                    break;
+                for (let i = 0; i < children.length; i++) {
+                    if (!$(children[i]).html()) {
+                        $(children[i]).html('Z <sub>' + task.getAttribute('id').substr(1) + '</sub>');
+                        break;
+                    }
                 }
-            }
 
-            if (tasks.length == 0) {
-                parent.html(k1);
-                parent.parent().show();
-                clearInterval(baer);
-            }
-        }, 1200, tasks, k1, k2);
-    }
-};
+                if (tasks.length === 0) {
+                    parent.html(k1);
+                    parent.parent().show();
+                    clearInterval(baer);
+                }
+            }, 1200, tasks, k1, k2);
+        }
+    };
 
-$(document).ready(function () {
-    Graph.init();
+    $(document).ready(function () {
+        Graph.init();
 
-    $(".baer-button").click(function () {
-        var tasks = $("circle");
+        $('.baer-button').click(function () {
+            let tasks = $('circle');
 
-        tasks.each(function (index, element) {
-            $(element).removeAttr("onclick", "");
+            tasks.each(function (index, element) {
+                $(element).removeAttr('onclick', '');
+            });
+
+            Graph.run(tasks, $('#p1'), $('#p2'), $('#c_max'));
+
+            $(this).fadeOut();
         });
-
-        Graph.run(tasks, $("#p1"), $("#p2"), $("#c_max"));
-
-        $(this).fadeOut();
     });
-});
+}());
